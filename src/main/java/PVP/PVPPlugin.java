805fc5a7d3e5787c23855df34ba1f267be39fc84
J-VdS <jeroen.vanderstraeten@ugent.be>
2019-09-03 20:43:06 +0200

    player cap

--- src/main/java/PVP/PVPPlugin.java
+++ src/main/java/PVP/PVPPlugin.java
@@ -12,7 +12,105 @@ import io.anuke.mindustry.plugin.Plugin;
 
 public class PVPPlugin extends Plugin{
     private boolean AdminOnly = false;
-    private int PlayerLimit = -1; //on starup, dus ongelimiteerd
+    private int playerLimit = -1; //on starup, dus ongelimiteerd
+
+    public PVPPlugin(){
+
+    }
+
+    //register commands that run on the server
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+        handler.register("playerlimit", "[new_limit]", "shows the limit or set the player limit (this will disable NEW connections)", args -> {
+            if (args.length == 0) {
+                if (this.playerLimit != -1) {
+                    Log.info("Current player limit: {0}", this.playerLimit);
+                } else {
+                    Log.info("Player limit disabled. This will only affect NEW connections");
+                }
+            } else {
+                if (!Strings.canParseInt(args[0])) return;
+                int newLimit = Strings.parseInt(args[0]);
+                this.playerLimit = (newLimit < 0) ? -1 : newLimit;
+                Log.info("Player limit changed.");
+            }
+        });
+    }
+
+    //register commands that player can invoke in-game
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+        handler.<Player>register("team", "", "Switch Teams", (args, player) ->{
+            if (!Vars.state.rules.pvp){
+                player.sendMessage("[scarlet]Only available in pvp.");
+                return;
+            } else if (!player.isAdmin && AdminOnly){
+                player.sendMessage("[accent]/team[][scarlet]disabled");
+                return;
+            }
+            //change team
+            changeTeam(player);
+
+        });
+
+        handler.<Player>register("ateam", "[playerId]", "[scarlet]AdminOnly[] Change the team of a player", (args, player) ->{
+           if (!player.isAdmin){
+               player.sendMessage("[scarlet]Admin only.");
+               return;
+           } else if (!Vars.state.rules.pvp){
+               player.sendMessage("[scarlet]Only available in pvp.");
+               return;
+           }
+            //TODO cleanup
+           if (args.length == 0){
+               //show player list
+               StringBuilder builder = new StringBuilder();
+               builder.append("[accent]ID:[]  name");
+               int ID = 1;
+               for (Player p : Vars.playerGroup.all()){
+                    if (p.con== null ) continue;
+                    builder.append("\n").append("[accent]").append(ID).append(": []").append(p.name);
+               }
+               player.sendMessage(builder.append("\n\nTo disable/enable [accent]/team[] use /ateam -1").toString());
+           } else {
+               try {
+                   int ID = Integer.parseInt(args[0]);
+                   if (ID == -1){
+                       this.AdminOnly = (this.AdminOnly) ? false : true;  //change state adminonly
+                       String msg = (!this.AdminOnly) ? "[geen]enabled[]" : "[scarlet]disabled[]";
+                       player.sendMessage("[accent]/team[] " + msg);
+                       return;
+                   }
+                   Player other = Vars.playerGroup.all().get(ID-1);
+                   changeTeam(other);
+                   player.sendMessage("[accent]changed the team of " + other.name);
+               } catch (Exception e){
+                   player.sendMessage("[scarlet]invalid ID");
+               }
+           }
+        });
+
+        handler.<Player>register("playerlimit", "[new_Limit]", "[scarlet]AdminOnly[] shows the limit or set a player limit (this will disable [accent]NEW[] connections)",
+                (args, player) ->{
+           if (!player.isAdmin){
+               player.sendMessage("[scarlet]Admin only!");
+               return;
+           }
+            if (args.length == 0) {
+                if (this.playerLimit != -1) {
+                    player.sendMessage("[accent]Current player limit: []" + this.playerLimit);
+                } else {
+                    player.sendMessage("[accent]Player limit disabled.[]");
+                }
+            } else {
+                if (!Strings.canParseInt(args[0])) return;
+                int newLimit = Strings.parseInt(args[0]);
+                this.playerLimit = (newLimit < 0) ? -1 : newLimit;
+                player.sendMessage("Player limit changed. This will only affect [accent]NEW[] connections");
+            }
+        });
+    }
 
     //change team
     private void changeTeam(Player player){
@@ -29,51 +127,5 @@ public class PVPPlugin extends Plugin{
         }
         //kill player
         Call.onPlayerDeath(player);
-    }
-
-    //register commands that player can invoke in-game
-    @Override
-    public void registerClientCommands(CommandHandler handler){
-
-        handler.<Player>register("team", "", "Switch Teams", (args, player) ->{
-            if (!player.isAdmin && AdminOnly){
-                player.sendMessage("[scarlet]Admin only.");
-                return;
-            }
-            //change team
-            if (!Vars.state.rules.pvp){
-                player.sendMessage("[scarlet]Only available in pvp.");
-                return;
-            }
-            changeTeam(player);
-
-        });
-
-        handler.<Player>register("ateam", "[playerId]", "[blue]AdminOnly[] Change the team of a player", (args, player) ->{
-           if (!player.isAdmin){
-               player.sendMessage("[scarlet]Admin only.");
-               return;
-           }
-           if (args.length == 0){
-               //show player list
-               StringBuilder builder = new StringBuilder();
-               int ID = 1;
-               for (Player p : Vars.playerGroup.all()){
-                    if (p.con== null ) continue;
-                    builder.append("[accent]").append(ID).append(": []").append(p.name).append("\n");
-               }
-               player.sendMessage(builder.toString());
-           } else {
-               if (Strings.canParseInt(args[0])){
-                   int index = Strings.parseInt(args[0])-1;
-                   Player other = Vars.playerGroup.all().get(index);
-                   if (other != null){
-                       changeTeam(other);
-                   } else {
-                       player.sendMessage("[scarlet]Invalid ID");
-                   }
-               }
-           }
-        });
     }
 }
